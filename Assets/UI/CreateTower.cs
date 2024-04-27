825e4581403e67ed3d1c0616ee9689543e6e1621
xxbenja16 <benja020201@live.dk>
2024-04-27 16:57:12 +0200

    Set towers

---
+++ Assets/UI/CreateTower.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CreateTower : MonoBehaviour
+{
+
+    public GameObject towerPrefab;
+
+    public Camera planetCamera;
+
+    public GameObject planet;
+
+    private GameObject spawnedTower;
+
+    private float correctionX = 25.82238f;
+    private float correctionY = 302.955f;
+    private bool isTowerSelected = false;
+
+    private void Start()
+    {
+        correctionX = planet.transform.position.x - correctionX;
+        correctionY = planet.transform.position.y - correctionY;
+    }
+
+    private void Update()
+    {
+        if (isTowerSelected)
+        {
+            Vector3 worldPosition = planetCamera.ScreenToWorldPoint(Input.mousePosition);
+            worldPosition.z = 0;
+            worldPosition.x += correctionX;
+            worldPosition.y += correctionY;
+
+            // Project it to the surface of the planet
+            Vector3 planetCenter = planet.transform.position;
+            Vector3 direction = worldPosition - planetCenter;
+
+            direction.Normalize();
+            worldPosition = planetCenter + direction * 5.3f;
+
+            spawnedTower.transform.position = worldPosition;
+            spawnedTower.transform.right = direction;
+
+            if (Input.GetMouseButtonDown(1))
+            {
+                Destroy(spawnedTower);
+                isTowerSelected = false;
+                spawnedTower = null;
+            }
+            if (Input.GetMouseButtonDown(0))
+            {
+                isTowerSelected = false;
+                spawnedTower = null;
+            }
+        }
+    }
+
+    public void SetTowerPrefab(GameObject towerPrefab)
+    {
+        this.towerPrefab = towerPrefab;
+        isTowerSelected = true;
+
+        Vector3 worldPosition = planetCamera.ScreenToWorldPoint(Input.mousePosition);
+        worldPosition.z = 0;
+        worldPosition.x += correctionX;
+        worldPosition.y += correctionY;
+
+        // Project it to the surface of the planet
+        Vector3 planetCenter = planet.transform.position;
+        Vector3 direction = worldPosition - planetCenter;
+
+        direction.Normalize();
+        worldPosition = planetCenter + direction * 5.3f;
+
+        spawnedTower = Instantiate(towerPrefab, worldPosition, Quaternion.identity);
+        spawnedTower.transform.right = direction;
+
+        spawnedTower.transform.localScale = new Vector3(1f, 1f, 1f);
+    }
+}
