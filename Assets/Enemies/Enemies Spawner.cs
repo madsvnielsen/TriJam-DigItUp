2cbd56efa64ca2143e736782a18e89dd9fc1cca6
xxbenja16 <benja020201@live.dk>
2024-04-27 18:31:18 +0200

    Restart game

--- Assets/Enemies/Enemies Spawner.cs
+++ Assets/Enemies/Enemies Spawner.cs
@@ -8,13 +8,21 @@ public class EnemiesSpawner : MonoBehaviour
     void Start()
     {
         InvokeRepeating("SpawnEnemy", 10f, 10f);
-        
+
     }
 
     void SpawnEnemy()
     {
-        Instantiate(Enemy, new Vector3(Random.Range(-10f,10f),310,0),Quaternion.identity);
+        Vector3 position = gameObject.transform.position;
+        // add a random vector to the position
+        Vector3 randomVector = new Vector3(20, 20, 0);
+
+        // rotate the vector by a random angle
+        float randomAngle = Random.Range(0, 360);
+        randomVector = Quaternion.Euler(0, 0, randomAngle) * randomVector;
+
+        GameObject newEnemy = Instantiate(Enemy, position + randomVector, Quaternion.identity);
     }
 
-    
+
 }
---
+++ Assets/GameOver.cs
@@ -0,0 +1,34 @@
+using TMPro;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+    public GameObject gameOverScreen;
+    public TMP_Text timerText; // Assign in Unity editor
+    public TMP_Text gameOverTimeText; // Assign in Unity editor
+
+    private float timer = 0.0f;
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        timerText.SetText(timer.ToString("0."));
+    }
+
+    public void RestartGame()
+    {
+        // Reload the scene
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            gameOverScreen.SetActive(true);
+            gameOverTimeText.gameObject.SetActive(true);
+            Time.timeScale = 0;
+            gameOverTimeText.SetText(timer.ToString("0.") + " seconds");
+        }
+    }
+}
